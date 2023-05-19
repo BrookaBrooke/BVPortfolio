55fe3e5752fae615fcbaa8e58b194af61abd9d9c
Brooke Vonderheid <bevfrisco@gmail.com>
2023-05-19 02:02:47 -0400

    started portfolio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
+import Portfolio from "./components/Portfolio";
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
   <NavBar />
   <Home />
   <About />
-
+  <Portfolio />
   <SocialLinks />
 </div>
   );
