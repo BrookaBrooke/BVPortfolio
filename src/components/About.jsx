593e6c6e1d9347edff50108eea848a472ae83400
Brooke Vonderheid <bevfrisco@gmail.com>
2023-05-29 13:07:52 -0400

    added about section but need to fix portfolio margins

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,25 +3,34 @@ const About = () => {
     return (
         <div
         name="about"
-        className="w-full h-screen  text-white"
+        className="w-full h-screen text-white "
         >
             <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
                 <div className="pb-8">
-                    <h1 className="text-4xl inline border-b-4 border-gray-500">About Me</h1>
+                    <h1 className="text-4xl font-bold inline border-b-4 border-gray-500">About Me</h1>
                 </div>
-                <p className="text-xl mt-20">
-                    Born in Dallas Texas, I began my career journey a few years ago, jumping from the
-                    field of customer service to the field of software engineering. My education includes
-                    engineering courswork from Arizona State University, as well as a certification of FullStack
-                    Web Development from Hack Reactor, where I learned frontend and backend developement, as well
-                    as the developement of REST APIs.
+                <p className="text-xl mt-5">
+                Hello! I'm [Your Name], a passionate and dedicated web developer with [X] years of experience. I thrive on transforming ideas into captivating and functional websites that provide seamless user experiences. With a strong background in [mention relevant technologies or programming languages], I am always eager to expand my skills and stay up-to-date with the latest trends in web development.
+                I specialize in developing responsive, dynamic, and user-friendly websites that meet the unique needs of clients. Here are some of my key areas of expertise:
                 </p>
 
                 <br/>
-                <p className="text-xl">
-                    I specialize in Python and Javascript based applications built with the Django and FastAPi Frameworks.
-                    Below listed are a few of my completed projects and examples of my abilities.
+                <p className="text-xl mt-5">
+                Regarding Front-End Development, I have a deep understanding of HTML, CSS, and JavaScript, which enables me to create visually appealing and interactive user interfaces. I am skilled in using modern frameworks like React.js to build efficient and scalable front-end solutions.
                 </p>
+
+                <br/>
+                <p className="text-xl mt-5">
+                As far as Back-End Development, I have a solid foundation in back-end technologies such as Node.js and Python, allowing me to develop robust and secure server-side applications. I am experienced in working with databases (SQL and NoSQL) and creating efficient APIs to enable seamless communication between the front and back ends.
+                </p>
+
+                <br/>
+                <p className="text-xl mt-5">
+                I prioritize creating websites that are accessible and visually appealing across various devices and screen sizes. Utilizing responsive design principles and frameworks like Bootstrap to ensure optimal user experiences on desktops, tablets, and smartphones.
+                </p>
+
+                <br/>
+
             </div>
         </div>
     )
